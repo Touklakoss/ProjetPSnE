627cfe1a4a2b99903a6ade0b2a16afa7187a0c59
Antoine LARGEAUD <antoine.largeaud@reseau.eseo.fr>
2023-10-16 10:40:49 +0200

    Branche de dev

--- foo/src/main.rs
+++ foo/src/main.rs
@@ -1,22 +1,9 @@
 #![allow(dead_code)]
 
-mod player;
-mod bubulle;
-mod displayer;
+mod GUI;
 
-use displayer::displayer::*;
+use GUI::GUI::*;
 
 fn main() {
-    println!("Hello, world!");
-
-    let bubulle = bubulle::bubulle::Bubulle::new(5.0, 100, String::from("Bulle 1"), 1);
-    bubulle.display_information();
-
-    let player1 = player::player::Player::new("Joueur1", "Host");
-    let player2 = player::player::Player::new("Joueur2", "Participant");
-    player1.display_information();
-    player2.display_information();
-
-    let mut displayer = Displayer::new();
-    displayer.render_window();
+    
 }
