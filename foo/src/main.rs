1197a10ead595bf729b99e76c1a1d864c542b9a3
Antoine LARGEAUD <antoine.largeaud@reseau.eseo.fr>
2023-11-01 20:25:58 +0100

    Probleme d'envoi des events

--- foo/src/main.rs
+++ foo/src/main.rs
@@ -1,9 +1,13 @@
 #![allow(dead_code)]
 
 mod GUI;
+mod game_core;
 
 use GUI::GUI::*;
+use game_core::game_core::*;
 
 fn main() {
-    
+    let mut gameCore = Game_core::new();
+
+    gameCore.game_core_run();
 }
